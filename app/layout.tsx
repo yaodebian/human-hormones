f5087e30a7962412515ee61d0196287fcbe29c96
yaodebian <2801540120@qq.com>
2025-04-29 13:04:43 +0800

    fix: 修复多巴胺详情页无法加载的问题

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import { LanguageProvider } from "@/lib/i18n/language-context";
 import { supportedLocales } from "@/middleware";
 
